e53cf4fb22258b8ae1392ed8bcc692083a59a59a
Asadbek Nosirjonov5566 <a.nosirjonov@uic.group>
2021-11-11 12:48:54 +0500

    feat: account div

--- components/Twit.vue
+++ components/Twit.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app" class="flex container h-screen w-full">
+
+    <!-- Side nav -->
     <div
       class="
         w-1/5
@@ -59,17 +61,34 @@
         </div>
         <!-- /Tweet btn -->
       </div>
-      <div class="w-full">
-        <button class="flex items-center w-full hover:bg-[#F5F8FA] rounded-full p-2">
-          <img src="../static/img/profile.png" alt="" class="w-12 h-12 rounded-full border border-[#E1E8ED] object-cover"/>
+      <div class="w-full relative">
+        <button @click="dropdown = true" class="flex items-center w-full hover:bg-[#EFF9FF] rounded-full p-2 focus:outline-none">
+          <img src="../static/img/profile.png" alt="" class="w-10 h-10 rounded-full border border-[#E1E8ED] object-cover"/>
           <div class="ml-4">
             <p class="text-sm font-bold leading-tight">Asadbek</p>
             <p class="text-sm leading-tight">@a.nosirjonov</p>
           </div>
           <i class="fas fa-angle-down ml-auto text-lg"></i>
         </button>
+        <div v-if="dropdown === true" class="absolute bottom-0 left-0 w-64 rounded-lg shadow-md border-[#F5F8FA] bg-white mb-16">
+          <button @click="dropdown = false" class="flex items-center w-full hover:bg-[#F5F8FA] p-3 focus:outline-none">
+            <img src="../static/img/profile.png" alt="" class="w-10 h-10 rounded-full border border-[#E1E8ED] object-cover"/>
+            <div class="ml-4">
+              <p class="text-sm font-bold leading-tight">Asadbek</p>
+              <p class="text-sm leading-tight">@a.nosirjonov</p>
+            </div>
+            <i class="fas fa-check ml-auto text-[#1DA1F2]"></i>
+          </button>
+          <button @click="dropdown = false" class="w-full text-left hover:bg-[#F5F8FA] border-t border-[#E1E8ED] p-3 text-sm focus:outline-none">
+            Add an existing account
+          </button>
+           <button @click="dropdown = false" class="w-full text-left hover:bg-[#F5F8FA] border-t border-[#E1E8ED] p-3 text-sm focus:outline-none">
+            Log out @a.nosirjonov
+          </button>
+        </div>
       </div>
     </div>
+    <!-- /Side nav -->
   </div>
 </template>
 <script>
@@ -85,6 +104,7 @@ export default {
         { icon: 'fas fa-clipboard-list', title: 'Lists', id: 'lists' },
       ],
       id: 'home',
+      dropdown: false
     }
   },
 }
